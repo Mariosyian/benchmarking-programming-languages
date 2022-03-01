6b754fbe1ed734bec8c7c4749f765d56bfd6d91f
Marios Yiannakou <marios.yiannakou@student.manchester.ac.uk>
2022-03-01 11:47:56 +0000

    Add isort to python task and format script. Use absolute paths where possible.

--- format.sh
+++ format.sh
@@ -3,6 +3,7 @@ echo "========== FORMAT CHECK =========="
 # Python (black)
 echo "Formatting python files using black"
 black .
+isort .
 # Rust (rustfmt)
 echo "Formatting rust files using rustfmt"
 rustfmt implementations/rust/**/*.rs
